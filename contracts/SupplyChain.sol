534bc2ea70b526550430def24cedc93d96dde35c
SatoshiSingh <23080389+SuperSuperSingh@users.noreply.github.com>
2019-01-11 19:11:41 +0200

    Consensys tests passing

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -89,7 +89,7 @@ contract SupplyChain {
     refunded any excess ether sent. Remember to call the event associated with this function!*/
 
     function buyItem(uint sku) public payable forSale(sku) paidEnough(items[sku].price) checkValue(sku) {
-        items[sku].seller.transfer(msg.value);
+        items[sku].seller.transfer(items[sku].price);
         items[sku].buyer = msg.sender;
         items[sku].state = State.Sold;
         emit Sold(sku);
